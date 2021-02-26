8bca74f53b44969e4f1ae999328397b22847c96f
wujiahan <2418173870@qq.com>
2021-02-26 16:36:42 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    int carrots;
+    carrots=25;
+    cout<<"I have ";
+    cout<<carrots;
+    cout<<" carrots.";
+    cout<<endl;
+    carrots=carrots-1;
+    cout<<"Now,I hava "<<carrots<<" carrots."<<endl;
+    return 0;
+}
